4c726159b0523db58a11b685901359ee1efc74aa
Lucas Rodrigues <contato.luccarodrigues@gmail.com>
2022-02-14 19:19:13 -0300

    refactor: refactor and implement new method for by requered fields

--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -4,11 +4,18 @@ import { httpRequest, httpResponse } from "../Protocols/http";
 
 export class SignUpController {
   handle(httpRequest: httpRequest): httpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError("name"));
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError("email"));
+    // if (!httpRequest.body.name) {
+    //   return badRequest(new MissingParamError("name"));
+    // }
+    // if (!httpRequest.body.email) {
+    //   return badRequest(new MissingParamError("email"));
+    // }
+
+    const requiredFields = ["name", "email", "password"];
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
     }
   }
 }
